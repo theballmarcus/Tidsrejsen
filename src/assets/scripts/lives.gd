253533b185fca18391bc909b3430dbee7a5d3585
Marcus <mcihme@gmail.com>
2025-10-20 14:38:42 +0200

    Fixed lives not being removed

--- src/assets/scripts/lives.gd
+++ src/assets/scripts/lives.gd
@@ -23,11 +23,11 @@ func _process(delta: float) -> void:
 		var idx = 0
 		for life in lives:
 			if Gamestate.health < idx + 1:
-				var new_texture = preload("res://assets/graphics/images/Health_Dot.png")
-				var l = life.get_node('Life')
-				l.texture = new_texture
+				life.modulate = Color(1,0,0) 
+				# var new_texture = preload("res://assets/graphics/images/Health_Dot.png")
+				# var l = life.get_node('Life')
+				# l.texture = new_texture
+				# l.visible = false
 			idx += 1
 		
 		last_life = Gamestate.health
-
-		
